1f28c5c0447880a1c54d6e3ec9ca07b1c0b8c7ee
ThomasRgbg <ThomasRgbg@users.noreply.github.com>
2023-01-21 07:55:24 +0100

    Initial checkin

---
+++ run_wetterdienst.py
@@ -0,0 +1,18 @@
+
+
+import dateutil
+import datetime
+import time
+
+from influxdb_cli2.influxdb_cli2 import influxdb_cli2
+from wetterdienst.wetterdienst_influxdb import wetterdienst_get
+
+from config_data import *
+
+influxdb = influxdb_cli2(influxdb_url, influxdb_token, influxdb_org, influxdb_bucket, debug=True)
+
+while True:
+
+    wetterdienst_get(influxdb)
+    
+    time.sleep(1 * 60 * 60)
---
+++ wetterdienst/wetterdienst_influxdb.py
@@ -0,0 +1,88 @@
+
+from numpy import datetime64, timedelta64, isnan
+import datetime
+
+from wetterdienst.provider.dwd.mosmix import DwdMosmixRequest, DwdMosmixType
+from wetterdienst import Settings
+
+
+def convert_dt64_to_dt(dt64):
+    unix_epoch = datetime64(0, 's')
+    one_second = timedelta64(1, 's')
+    seconds_since_epoch = (dt64 - unix_epoch) / one_second
+    return datetime.datetime.utcfromtimestamp(seconds_since_epoch)
+
+
+def wetterdienst_get(influxdb):
+
+    Settings.tidy = True
+
+    request = DwdMosmixRequest(
+        mosmix_type=DwdMosmixType.SMALL,
+        parameter=["TTT", 'FF', 'RR1c', 'Neff', 'SunD1', 'Rad1h', 'VV', 'wwM'],
+        start_date=(datetime.date.today().isoformat()),
+        end_date=((datetime.date.today()  + datetime.timedelta(7)).isoformat()),
+    )
+
+    stations = request.filter_by_station_id(station_id=["P551", "10776", "P366", "P441", "P449", "P460", "N0680"],)
+
+    for response in stations.values.query():
+
+        print(dir(response))
+        print(response)
+
+    # next2days = response.df[0:48]
+
+        for i in range(len(response.df)):
+            
+            # print(response.df["parameter"].values[i])
+            # continue
+            
+            value = response.df["value"].values[i] 
+
+            if isnan(value):
+                continue
+
+            date = response.df["date"].values[i]
+            if response.df["station_id"].values[i] == "10776":
+                location = "weatherforcast"
+            elif response.df["station_id"].values[i] == "P551":
+                location = "weatherforcast_langquaid"
+            elif response.df["station_id"].values[i] == "P460":
+                location = "weatherforcast_hagelstadt"
+            elif response.df["station_id"].values[i] == "P366":
+                location = "weatherforcast_schwandorf"
+            elif response.df["station_id"].values[i] == "P441":
+                location = "weatherforcast_parsberg"
+            elif response.df["station_id"].values[i] == "P449":
+                location = "weatherforcast_kelheim"
+            elif response.df["station_id"].values[i] == "N0680":
+                location = "weatherforcast_germersheim"
+            else:
+                continue
+            
+            if response.df["parameter"].values[i] == 'temperature_air_mean_200':
+                influxdb.write_sensordata(location, "temperature_2m", value - 273.15, convert_dt64_to_dt(date))
+
+            if response.df["parameter"].values[i] == 'wind_speed':
+                influxdb.write_sensordata(location, "windspeed", value * 3.6, convert_dt64_to_dt(date))
+
+            if response.df["parameter"].values[i] == 'precipitation_height_significant_weather_last_1h':
+                influxdb.write_sensordata(location, "rain", value, convert_dt64_to_dt(date))
+
+            if response.df["parameter"].values[i] == 'cloud_cover_effective':
+                influxdb.write_sensordata(location, "cloudcover", value, convert_dt64_to_dt(date))
+
+            if response.df["parameter"].values[i] == 'sunshine_duration':
+                influxdb.write_sensordata(location, "sunshine", value, convert_dt64_to_dt(date))
+
+            if response.df["parameter"].values[i] == 'radiation_global':
+                influxdb.write_sensordata(location, "radiation_all", value, convert_dt64_to_dt(date))
+
+            if response.df["parameter"].values[i] == 'visibility_range':
+                influxdb.write_sensordata(location, "visibility", value, convert_dt64_to_dt(date))
+
+            if response.df["parameter"].values[i] == 'probability_fog_last_1h':
+                influxdb.write_sensordata(location, "propability_fog", value, convert_dt64_to_dt(date))
+
+    print("Done")
