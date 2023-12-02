6ef089224185e7ba5e85107b54f8b70ab3c20770
ThomasRgbg <ThomasRgbg@users.noreply.github.com>
2023-12-02 23:00:50 +0100

    ...

--- run_solcast.py
+++ run_solcast.py
@@ -6,6 +6,7 @@ import time
 
 from influxdb_cli2.influxdb_cli2 import influxdb_cli2
 from solcast.solcast_get import solcast_get
+from pysolcast import exceptions
 
 from config_data import *
 
@@ -15,7 +16,7 @@ while True:
 
     try:
         solcast_get(solcast_api_key, solcast_resource_id, influxdb)
-    except pysolcast.exceptions.RateLimitExceeded:
+    except exceptions.RateLimitExceeded:
         time.sleep(4 * 60 * 60)
     
     time.sleep(2 * 60 * 60)
